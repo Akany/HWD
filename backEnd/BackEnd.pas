b5eb0607128d2f99e486449c9963384d9f475d37
akany <sif.mori@gmail.com>
2013-05-01 22:12:40 +0300

    added md5

--- backEnd/BackEnd.pas
+++ backEnd/BackEnd.pas
@@ -19,25 +19,24 @@ type
 
 var
   Form1: TForm1;
-//  TKeyBit: (kb128, kb192, kb256);
 
 implementation
 
 {$R *.dfm}
 
-function getHWInfo () : String; external 'HardwareInfo.dll';
-function EncryptString (Value: PChar; Key: PChar;
-  KeyBit: TKeyBit = kb128): PChar; stdcall; external 'AESDLL.dll';
+function getHWInfo () : Pchar; external 'HardwareInfo.dll';
+//function EncryptString (Value: PChar; Key: PChar;
+//  KeyBit: TKeyBit = kb128): PChar; stdcall; external 'AESDLL.dll';
 
 procedure TForm1.FormCreate(Sender: TObject);
 var
   key: String;
-  criptValue : Pchar;
+  //criptValue: Pchar;
 begin
-  key := 'test';
-  criptValue := EncryptString(pansichar(Trim(getHWInfo())), pansichar(key), kb256);
+  //key := 'test';
+  //criptValue := EncryptString(pansichar(Trim(getHWInfo())), pansichar(key));
   HWInfo.Clear();
-  HWInfo.Lines.Add(criptValue);
+  HWInfo.Lines.Add(getHWInfo());
 end;
 
 end.
--- hardwareInfo/hddId.pas
+++ hardwareInfo/hddId.pas
@@ -1,20 +1,9 @@
-library HardwareInfo;
+unit hddId;
 
-{ Important note about DLL memory management: ShareMem must be the
-  first unit in your library's USES clause AND your project's (select
-  Project-View Source) USES clause if your DLL exports any procedures or
-  functions that pass strings as parameters or function results. This
-  applies to all strings passed to and from your DLL--even those that
-  are nested in records and classes. ShareMem is the interface unit to
-  the BORLNDMM.DLL shared memory manager, which must be deployed along
-  with your DLL. To avoid using BORLNDMM.DLL, pass string information
-  using PChar or ShortString parameters. }
+INTERFACE
 
 uses
-  Windows, Messages, SysUtils, Classes, Graphics, Controls, Dialogs,
-  StdCtrls, md5;
-
-{$R *.res}
+   Windows, SysUtils;
 
 {getting HDD serial number}
 const
@@ -81,6 +70,10 @@ type
     dwReserved: array[1..4] of DWORD;
   end;
 
+function getHddId() : String;
+
+IMPLEMENTATION
+
 procedure CorrectDevInfo(var _params: TSendCmdOutParams);
 asm
   lea   edi, _params.bBuffer
@@ -142,61 +135,4 @@ begin
   end;
 end;
 
-{Getting computer name}
-function _getComputerName: String;
-var
- Buffer: array[0..255] of Char;
- Size: DWORD;
-begin
- size := 256;
- if GetComputerName(Buffer, Size) then
-   Result := Buffer
- else
-   Result := ''
-end;
-
-{Getting user name}
-function _getUserName: String;
-var
- Size: Cardinal;
- PRes: PChar;
- BRes: Boolean;
-begin
- Size := MAX_COMPUTERNAME_LENGTH + 1;
- PRes := StrAlloc(Size);
- BRes := GetUserName(PRes, Size);
- if BRes then
-   Result := StrPas(PRes)
- else
-   Result := '';
-end;
-
-function concatHWInfo (hddInfo, compName, userName : String) : String;
-begin
-  Result := hddInfo + '-' + compName + '-' + userName;
-end;
-
-function convertHashToPchar (hashArray: MD5Digest) : Pchar;
-var
-  i: integer;
-  c: String;
-begin
-  for  i := 0 to Length(hashArray) - 1 do
-    c := c + IntToHex(hashArray[i], 2);
-  Result := pansichar(c);
-end;
-
-{exported function
-getting HardWare info}
-function getHWInfo () : Pchar;
-var
-  HWInfo: String;
-begin
-  HWInfo := Trim(concatHWInfo(_getComputerName(), getHddId(), _getUserName()));
-  Result := convertHashToPchar(md5String(HWInfo));
-end;
-
-exports getHWInfo;
-
-begin
 end.
